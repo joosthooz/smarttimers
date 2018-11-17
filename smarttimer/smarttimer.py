6ac28fabb0fd85e59997b2320c76235d4e43533b
Eduardo Ponce <edponce2010@gmail.com>
2018-11-17 04:48:50 -0500

    added SmartTimer class template

---
+++ smarttimer/smarttimer.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+"""SmartTimer
+
+Classes:
+
+    SmartTimer
+"""
+
+
+from .timer import Timer
+
+
+__all__ = ['SmartTimer']
+
+
+class SmartTimer:
+    """Class for representing a SmartTimer."""
+
+    def __init__(self):
+        pass
