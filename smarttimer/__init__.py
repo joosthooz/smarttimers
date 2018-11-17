2d35b856db83501c10763bb5c8c929d42165488a
Eduardo Ponce <edponce2010@gmail.com>
2018-11-17 04:50:05 -0500

    created smarttimer module

---
+++ smarttimer/__init__.py
@@ -0,0 +1,26 @@
+"""SmartTimer module."""
+
+
+from .timer import Timer
+from .smarttimer import SmartTimer
+
+
+__all__ = [
+    'Timer',
+    'SmartTimer',
+    ]
+
+
+__title__ = "SmartTimer"
+__name__ = "SmartTimer"
+__version__ = "0.2.0"
+__description__ = "SmartTimer library"
+__keywords__ = [
+    "time",
+    "profiling"
+    ]
+__url__ = "https://github.com/edponce/smarttimers"
+__author__ = "Eduardo Ponce, The University of Tennessee, Knoxville, TN"
+__author_email__ = "edponce2010@gmail.com"
+__license__ = "MIT"
+__copyright__ = "2018 Eduardo Ponce"
