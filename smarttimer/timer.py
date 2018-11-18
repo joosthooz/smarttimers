3664dc270fccbe5c2a9db8a6105ab7394e84686d
Eduardo Ponce <edponce2010@gmail.com>
2018-11-17 23:01:19 -0500

    changed mode-time to clock_name-clock, improved show_clocks() to use ClockInfo, and changes for flake8 compliance

--- smarttimer/timer.py
+++ smarttimer/timer.py
@@ -13,6 +13,14 @@ import time
 __all__ = ['Timer']
 
 
+class ClockInfo:
+    def __init__(self, **kwargs):
+        self.adjustable = kwargs.get('adjustable', None)
+        self.implementation = kwargs.get('implementation', None)
+        self.resolution = kwargs.get('resolution', None)
+        self.monotonic = kwargs.get('monotonic', None)
+
+
 class Timer:
     """Class to read current time from a supported clock/counter.
 
@@ -28,19 +36,20 @@ class Timer:
 
     Attributes:
         id (str, optional): Timer identifier.
-        seconds (float, optional): Time in fractional seconds.
-        minutes (float): Time in fractional minutes (read-only).
-        mode (int, optional): Time mode used to index **TIME_MODES** map to
-                              select a time measurement function.
-        DEFAULT_TIME_MODE (int): Default time mode.
-        TIME_MODES (dict of int -> function): Map of time modes and time
-                                              measurement functions.
+        seconds (float, optional): Time measured in fractional seconds.
+        minutes (float, read-only): Time measured in fractional minutes.
+        clock_name (str, optional): Clock name used to index the **CLOCKS**
+                                    map to select a time measurement function.
+        DEFAULT_CLOCK_NAME (str): Default clock name.
+        CLOCKS (dict of str -> func): Map of clock names and time measurement
+                                      functions.
 
     Note:
         * **minutes** is read-only and set automatically when **seconds** is
-          modified. This ensures consistency between time values.
-        * When registering a new timing function, the user has freedom of which
-          time mode to use (add new mode value or overwrite an existing mode).
+          modified. This ensures consistency between timing values.
+        * When registering a new timing function to **CLOCKS**, it is
+          recommended to use a unique clock name to prevent overwriting over an
+          existing one.
 
     Todo:
         * Fix attributes and property documentation, attributes appear twice.
@@ -76,83 +85,101 @@ class Timer:
         **Example**::
 
             def custom_time_function(*args, **kwargs):
-                # Measure time ...
-                time_in_some_unit = 0.
+                # Measure time
+                time_in_some_unit = ...
 
-                # Convert time to fractional seconds ...
-                time_seconds = 0.
+                # Convert time to fractional seconds
+                time_seconds = time_in_some_unit ...
                 return time_seconds
 
-            # Register custom_time_function() to mode 5
-            >>> Timer.TIME_MODES[5] = custom_time_function
+            # Register custom_time_function() with clock name 'custom_time'
+            >>> Timer.CLOCKS['custom_time'] = custom_time_function
     """
 
-    DEFAULT_TIME_MODE = 0
-    TIME_MODES = {
-        0: time.perf_counter,
-        1: time.process_time,
-        2: time.clock,
-        3: time.monotonic,
-        4: time.time
+    DEFAULT_CLOCK_NAME = 'perf_counter'
+    CLOCKS = {
+        'perf_counter': time.perf_counter,
+        'process_time': time.process_time,
+        'clock': time.clock,
+        'monotonic': time.monotonic,
+        'time': time.time
     }
 
-    def __init__(self, id='', seconds=0., mode=-1):
+    def __init__(self, id='', **kwargs):
         """Constructor for Timer class.
 
         The **seconds** argument is used as the initial time measured.
-        The attribute **minutes** is set automatically by the **seconds** setter
-        method.
+        The attribute **minutes** is set automatically by the **seconds**
+        setter method.
         """
         self.id = id
-        self.seconds = seconds
-        self.mode = mode
+        self.seconds = kwargs.get('seconds', 0.)
+        self.clock_name = kwargs.get('clock_name', '')
 
     @classmethod
-    def show_modes(cls):
-        """Print available time modes and time measurement functions."""
-        for k, v in cls.TIME_MODES.items():
-            print(k,v)
-        print("Default mode is {}".format(cls.DEFAULT_TIME_MODE))
+    def show_clocks(cls):
+        """Print info of available clocks.
+
+        For **clock_names** that can be queried with *time.get_clock_info*,
+        detailed attributes are printed. All other cases only print the
+        corresponding timing function.
+        """
+        for n, f in cls.CLOCKS.items():
+            try:
+                info = time.get_clock_info(n)
+            except (TypeError, ValueError):
+                info = ClockInfo(implementation=f.__name__ + '()')
+
+            print("{name}:\n"
+                  "   function      : {func}\n"
+                  "   adjustable    : {info.adjustable}\n"
+                  "   implementation: {info.implementation}\n"
+                  "   monotonic     : {info.monotonic}\n"
+                  "   resolution    : {info.resolution}\n"
+                  .format(name=n, func=f, info=info))
+
+        print("Default clock is {}".format(cls.DEFAULT_CLOCK_NAME))
 
     @property
-    def mode(self):
-        """Get or set the time mode.
+    def clock_name(self):
+        """Get or set the clock name.
 
-        The time **mode** indexes the **TIME_MODES** mapping to select a time
-        measurement function. If **mode** is set to a negative value then
-        **DEFAULT_TIME_MODE** is used.
+        **clock_name** indexes the **CLOCKS** map to select a time measurement
+        function. If **clock_name** is set to the empty string then
+        **DEFAULT_CLOCK_NAME** is used.
 
         **Example**::
 
             # Find the timing function of a Timer
             >>> t = Timer()
-            >>> Timer.TIME_MODES[t.mode]
+            >>> Timer.CLOCKS[t.mode]
             # or
-            >>> Timer.show_modes()
-            >>> t.mode
+            >>> Timer.show_clocks()
+            >>> t.clock_name
 
-            # Change the time mode of a Timer
+            # Change the current clock of a Timer
             >>> t = Timer()
-            >>> t.mode = 3
+            >>> t.clock_name = 'process_time'
 
         Args:
-            mode (int): Time mode used to select a time measurement function.
+            clock_name (str): Clock name used to select a timing function.
 
         Returns:
-            int: Time mode.
+            str: Current clock name.
 
         Raises:
-            TypeError: If **mode** is not an integer.
+            TypeError: If **clock_name** is not a string.
         """
-        return self._mode
+        return self._clock_name
 
-    @mode.setter
-    def mode(self, mode):
-        if not isinstance(mode, int):
-            raise TypeError("Invalid data type for 'mode' attribute, '{}', "
-                            "requires an integer.".format(type(mode)))
-        self._mode = mode if mode >= 0 else type(self).DEFAULT_TIME_MODE
-        self._time_func = type(self).TIME_MODES[self._mode]
+    @clock_name.setter
+    def clock_name(self, clock_name):
+        if not isinstance(clock_name, str):
+            raise TypeError("Invalid type for 'clock_name' attribute, '{}', "
+                            "requires a string.".format(type(clock_name)))
+        self._clock_name = clock_name if clock_name \
+            else type(self).DEFAULT_CLOCK_NAME
+        self._clock = type(self).CLOCKS[self._clock_name]
 
     @property
     def id(self):
@@ -172,8 +199,8 @@ class Timer:
     @id.setter
     def id(self, id):
         if not isinstance(id, str):
-            raise TypeError("Invalid data type for 'id' attribute, '{}', "
-                            "requires a string.".format(type(id)))
+            raise TypeError("Invalid type for 'id' attribute, '{}', requires "
+                            "a string.".format(type(id)))
         self._id = id
 
     @property
@@ -198,7 +225,7 @@ class Timer:
     @seconds.setter
     def seconds(self, seconds):
         if not isinstance(seconds, (int, float)):
-            raise TypeError("Invalid data type for 'seconds' attribute, '{}', "
+            raise TypeError("Invalid type for 'seconds' attribute, '{}', "
                             "requires a number".format(type(seconds)))
         if seconds < 0.:
             raise ValueError("Invalid value for 'seconds' attribute, '{}', "
@@ -219,15 +246,20 @@ class Timer:
         """String representation of a Timer.
 
         Returns:
-            str: comma delimited string, format is **seconds**, **minutes**,
-                 **id**.
+            str: Comma delimited string, format is **seconds**, **minutes**,
+                 **id**. If **id** is empty it is ignored.
         """
-        return "{:.4f}, {:.4f}, {}".format(self.seconds, self.minutes, self.id)
+        if self.id:
+            return "{:>10.4f}, {:>10.4f}, {:>10}".format(self.seconds,
+                                                         self.minutes,
+                                                         self.id)
+        else:
+            return "{:>10.4f}, {:>10.4f}".format(self.seconds, self.minutes)
 
     def time(self, *args, **kwargs):
         """Invoke time measurement function and record measured time.
 
-        Calls timing function currently configured via time **mode**.
+        Calls timing function currently configured via **clock_name**.
         This method accepts arbitrary positional and/or keyword arguments to
         enable support for arbitrary signatures of timing functions.
 
@@ -238,12 +270,16 @@ class Timer:
         Returns:
             float: Time measured in fractional seconds.
         """
-        self.seconds = self._time_func(*args, **kwargs)
+        self.seconds = self._clock(*args, **kwargs)
         return self.seconds
 
     @classmethod
     def are_compatible(cls, t1, t2):
-        """Check if Timer pair use the same timing function.
+        """Check if Timer pair have same attributes.
+
+        For **clock_names** that can be queried with *time.get_clock_info*,
+        compatibility requires that all attributes are identical. All other
+        cases require that the timing functions are the same function.
 
         Args:
             t1 (Timer): First Timer.
@@ -252,7 +288,11 @@ class Timer:
         Returns:
             bool: True if Timer pair are compatible, else False
         """
-        return cls.TIME_MODES[t1.mode] is cls.TIME_MODES[t2.mode]
+        try:
+            return time.get_clock_info(t1.clock_name) == \
+                       time.get_clock_info(t2.clock_name)
+        except (TypeError, ValueError):
+            return cls.CLOCKS[t1.clock_name] is cls.CLOCKS[t2.clock_name]
 
     @classmethod
     def compatibility_test(cls, t1, t2):
@@ -296,7 +336,7 @@ class Timer:
         """
         cls.compatibility_test(t1, t2)
         new_id = '-'.join(filter(None, [t1.id, t2.id]))
-        return Timer(new_id, abs(t1.seconds - t2.seconds))
+        return Timer(new_id, seconds=abs(t1.seconds - t2.seconds))
 
     @classmethod
     def add(cls, t1, t2):
@@ -325,7 +365,7 @@ class Timer:
         """
         cls.compatibility_test(t1, t2)
         new_id = '+'.join(filter(None, [t1.id, t2.id]))
-        return Timer(new_id, t1.seconds + t2.seconds)
+        return Timer(new_id, seconds=t1.seconds + t2.seconds)
 
     def __add__(self, other):
         """See **add()** method."""
