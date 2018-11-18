d8abd796cf5a4ddb9fed036c68ba5931515f0d35
Eduardo Ponce <edponce2010@gmail.com>
2018-11-17 22:42:39 -0500

    changes for flake8 compliance

--- setup.py
+++ setup.py
@@ -50,24 +50,25 @@ extras_requirements = {
 # Github hosting creates tarballs for download at
 #   https://github.com/{username}/{package}/archive/{tag}.tar.gz.
 # To create a git tag
-#   git tag pkg.__name__-pkg.__version__ -m 'Adds a tag so that we can put package on PyPI'
+#   git tag pkg.__name__-pkg.__version__ -m 'Adds a tag so that we can put
+#                                            package on PyPI'
 #   git push --tags origin master
 setup(
-    name = pkg.__name__,
-    version = pkg.__version__,
-    description = pkg.__description__,
-    long_description = long_description,
-    keywords = pkg.__keywords__,
-    url = pkg.__url__,
-    download_url = '{}/archive/{}-{}.tar.gz'.format(pkg.__url__,
-                                                    pkg.__name__,
-                                                    pkg.__version__),
-    author = pkg.__author__,
-    author_email = pkg.__author_email__,
-    license = pkg.__license__,
-    classifiers = [
+    name=pkg.__name__,
+    version=pkg.__version__,
+    description=pkg.__description__,
+    long_description=long_description,
+    keywords=pkg.__keywords__,
+    url=pkg.__url__,
+    download_url='{}/archive/{}-{}.tar.gz'.format(pkg.__url__,
+                                                  pkg.__name__,
+                                                  pkg.__version__),
+    author=pkg.__author__,
+    author_email=pkg.__author_email__,
+    license=pkg.__license__,
+    classifiers=[
         'Development Status :: 3 - Alpha',
-#        'Development Status :: 5 - Production/Stable',
+        # 'Development Status :: 5 - Production/Stable',
         'Environment :: Console',
         'Intended Audience :: Developers',
         'Intended Audience :: Science/Research',
@@ -80,14 +81,14 @@ setup(
         'Topic :: Documentation :: Sphinx',
         'Topic :: Software Development :: Libraries',
         ],
-    platforms = ['Linux'],
-    zip_safe = False,
-    python_requires = '>=3.3',
-    include_package_data = True,
+    platforms=['Linux'],
+    zip_safe=False,
+    python_requires='>=3.3',
+    include_package_data=True,
     packages=find_packages(),
-    install_requires = install_requirements,
-    setup_requires = setup_requirements,
-    extras_require = extras_requirements,
-    tests_require = tests_requirements,
-    test_suite = 'tests'
+    install_requires=install_requirements,
+    setup_requires=setup_requirements,
+    extras_require=extras_requirements,
+    tests_require=tests_requirements,
+    test_suite='tests'
 )
