1270936eea9839a2c2b2301b071c3bae3f3dfa5a
Eduardo Ponce <edponce2010@gmail.com>
2018-11-22 05:59:37 -0500

    set osx tests to py3.4 (and smarttimers), added py3.4 test to linux

--- smarttimer/__init__.py
+++ smarttimer/__init__.py
@@ -8,18 +8,18 @@ from .smarttimer import SmartTimer
 __all__ = [
     'Timer',
     'SmartTimer',
-    ]
+]
 
 
 __title__ = "SmartTimer"
 __name__ = "SmartTimer"
-__version__ = "0.4.0"
+__version__ = "0.5.0"
 __description__ = "SmartTimer library"
 __keywords__ = [
     "time",
     "profiling",
     "walltime"
-    ]
+]
 __url__ = "https://github.com/edponce/smarttimers"
 __author__ = "Eduardo Ponce, The University of Tennessee, Knoxville, TN"
 __author_email__ = "edponce2010@gmail.com"
