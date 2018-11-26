c8c2fbff6e21da66add1720f8e27b129bc0be50c
Eduardo Ponce <edponce2010@gmail.com>
2018-11-25 20:05:39 -0500

    stable SmartTimer, corrected remove(), and added write_to_file()

--- smarttimer/smarttimer.py
+++ smarttimer/smarttimer.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python3
-
 """SmartTimer
 
 Classes:
@@ -7,64 +5,107 @@ Classes:
 """
 
 
-import numpy
 from .timer import Timer
+from .exceptions import TimerTypeError
 
 
 __all__ = ['SmartTimer']
 
 
 class SmartTimer:
-    """Timer that tracks multiple timed regions.
+    """Data structure for timing code blocks.
 
-    Supports consecutive and nested timed regions.
-    Consecutive timer: tic(), toc(), ..., tic(), toc()
-    Nested timer:      tic(), tic(), ..., toc(), toc()
+    Supports consecutive and nested timed regions:
+        * Consecutive timer: tic(), toc(), ..., tic(), toc()
+        * Nested timer:      tic(), tic(), ..., toc(), toc()
+        * Cascade timer:     tic(), toc(), toc(), ...
 
     Contains time of last event and list of all events (supports indexing).
-
-    Todo:
-        * Use id attribute
     """
-    def __init__(self, id='', **kwargs_t):
-        self.id = id
-        self._timers = []
-        self._timer_stack = []  # stack of Time objects for active code blocks
-        self._kwargs_t = kwargs_t
+    def __init__(self, name='smarttimer', **kwargs):
+        self._name = name
+        self._timers = []  # list of Timers for completed time blocks
+        self._timer_stack = []  # stack of Timers for active time blocks
+#        self._timer_order = []  # list for ordering Timers based on tic()
+
+        # Remove 'label', not needed for internal timer
+        if 'label' in kwargs:
+            del kwargs['label']
+
+        # _timer: Internal Timer to record time
+        # _last_tic: Pointer used to support cascade scheme
+        self._last_tic = self._timer = Timer(**kwargs)
 
     @property
-    def timers(self):
-        return self._timers
+    def name(self):
+        return self._name
 
-    def get_times(self, key=slice(None)):
-        """Return times as Numpy array.
+    @name.setter
+    def name(self, name):
+        if not isinstance(name, str):
+            raise TimerTypeError('name', str)
+        self._name = name
+
+    @property
+    def labels(self):
+        """Lists of time labels for completed blocks.
+
+        Returns:
+            list: Labels of completed blocks.
         """
-        if not self.timers:
-            return numpy.empty(shape=0)
+        return [t.label for t in self._timers]
 
-        data = self.timers[key]
-        times = numpy.empty(shape=(len(data), 2))
-        for i, t in enumerate(data):
-            times[i] = t.seconds, t.minutes
-        return times
+    @property
+    def labels_active(self):
+        """Lists of time labels for active blocks.
+
+        Returns:
+            list: Labels of blocks without a matching toc().
+        """
+        return [t.label for t in self._timer_stack]
+
+    @property
+    def seconds(self):
+        """List of seconds elapsed for completed blocks.
+
+        Returns:
+            list: Time in seconds.
+        """
+        return [t.seconds for t in self._timers]
+
+    @property
+    def minutes(self):
+        """List of minutes elapsed for completed blocks.
+
+        Returns:
+            list: Time in minutes.
+        """
+        return [t.minutes for t in self._timers]
+
+    @property
+    def times(self):
+        """Dictionary of times elapsed for completed blocks.
+
+        Returns:
+            list: Keys are labels and values list of time in seconds.
+        """
+        time_map = {}
+        for k, v in zip(self.labels[0], self.seconds):
+            if k not in time_map:
+                time_map[k] = [v]
+            else:
+                time_map[k].append(v)
+        return time_map
 
     def __enter__(self):
-        """
-        Measure time at the latest moment possible to not include internal
-        processing tasks.
-        """
         self.tic()
         return self
 
     def __exit__(self, *args):
-        """
-        Measure time at the soonest moment possible to not include internal
-        processing tasks.
-        """
         self.toc()
 
     def __str__(self):
-        return '\n'.join([t.__str__() for t in self.timers])
+        return '\n'.join([t.__str__() for t in self._timers]) + '\n'
 
     def __getitem__(self, *keys):
         """Use dictionary syntax to access time in seconds
@@ -73,96 +114,148 @@ class SmartTimer:
         Multiple values are returned as a list, generally in the same order
         as the given keys (except for cases where duplicate labels exist).
 
-        Todo:
-            * Fix this method, support slices
+        Note:
+            * Remove from timers, not stack
 
-        Notes:
+        Note:
             * If key is string, then consider it as a label
             * If key is integer, then consider it as an index
+            * If key is dict, then dict.key is used as the actual key
             * Key can be an iterable of integers and/or strings
 
         Example:
             timer[4, 2]
             timer['I/O', 'Processing']
-            timer['I/O', 2]
+            timer[2, 'Processing']
+            timer[1:2, 4:6]
+            timer[['I/O', 'Processing'], [1,2]]
         """
-        data = []
-        for key in keys:
-            try:
-                if isinstance(key, slice):
-                    return self.timers
+        seconds = []
+        try:
+            for key in keys:
+                # Ensure 'key' is a single level iterable to allow loop
+                # processing. When an iterable or multiple keys are passed,
+                # the arguments are automatically organized as a tuple of
+                # tuple of values ((arg1,arg2),).
+                # Handle single strings because they are iterable.
+                if not hasattr(key, '__iter__') or isinstance(key, str):
+                    key = [key]
 
-                else:
-                    # Ensure 'key' is a single level iterable to allow loop processing.
-                    # When an iterable or multiple keys are passed, the arguments are
-                    # automatically organized as a tuple of tuple of values
-                    # ((arg1,arg2),)
-                    if not hasattr(key, '__iter__'):
-                        key = [key]
-
-                    # str, int: returns a list of Timers [t1,t2,...]
-                    if all(isinstance(k, str) for k in key):
-                        data = [self.timers[i] for i, k in enumerate(key) if k == self.timers[i].id]
-                    elif all(isinstance(k, int) for k in key):
-                        data = [self.timers[k] for k in key]
+                for k in key:
+                    if isinstance(k, slice):
+                        seconds.append(self.seconds[k])
+                    elif isinstance(k, int):
+                        seconds.append(self._timers[k].seconds)
+                    elif isinstance(k, str):
+                        for t in self._timers:
+                            if k == t.label:
+                                seconds.append(t.seconds)
                     else:
                         raise KeyError('Invalid key \'{}\' for {} '
                                        'object'.format(key, repr(self)))
-            except (IndexError, ValueError) as ex:
-                print(ex)
+        except (IndexError, ValueError) as ex:
+            print(ex)
 
-        # Return the items when there is only a single item
-        # This is important for supporting vector arithmetic.
-        if len(data) == 1: data = data[0]
+        if not seconds:
+            return None
+        else:
+            return seconds if len(seconds) > 1 else seconds[0]
 
-        return data
-
-    def __add__(self, other):
-        return self.walltime() + other.walltime()
-
-    def remove(self, key):
+    def remove(self, *keys):
         """Remove a Timer.
         Note:
             * Remove from timers, not stack
-        """
-        if isinstance(key, int):
-            self.timers.remove(self.timers[key])
-        elif isinstance(key, str):
-            for t in self[key]:
-                self.timers.remove(t)
 
-    def clear():
-        pass
+        Note:
+            * If key is string, then consider it as a label
+            * If key is integer, then consider it as an index
+            * If key is dict, then dict.key is used as the actual key
+            * Key can be an iterable of integers and/or strings
+        """
+        try:
+            for key in keys:
+                # Ensure 'key' is a single level iterable to allow loop
+                # processing. When an iterable or multiple keys are passed,
+                # the arguments are automatically organized as a tuple of
+                # tuple of values ((arg1,arg2),).
+                # Handle single strings because they are iterable.
+                if not hasattr(key, '__iter__') or isinstance(key, str):
+                    key = [key]
+
+                for k in key:
+                    if isinstance(k, slice):
+                        for t in self._timers[key]:
+                            self._timers.remove(t)
+                    elif isinstance(k, int):
+                        self._timers.remove(self._timers[k])
+                    elif isinstance(k, str):
+                        # Need a copy of _timers because it is modified
+                        for t in self._timers[:]:
+                            if k == t.label:
+                                self._timers.remove(t)
+                    else:
+                        raise KeyError('Invalid key \'{}\' for {} '
+                                       'object'.format(key, repr(self)))
+        except (IndexError, ValueError) as ex:
+            print(ex)
+
+    def clear(self):
+        """Set time values to zero."""
+        self._timers = []
+        self._timer_stack = []
+        self._timer.clear()
+        self._last_tic = self._timer
 
     def walltime(self):
+        """Compute walltime in seconds.
         """
-        Compute walltime by adding all timings measured.
-        Walltime is computed on-demand because it is not expected to be
-        invoked multiple times, so to reduce internal processing tasks, a
-        running sum is not used.
-        """
-        return sum([t.seconds for t in self.timers])
+        return sum(self.seconds)
 
     def tic(self, key=''):
-        t1 = Timer(id=key, **self._kwargs_t)
-        t1.time()
-        self._timer_stack.append(t1)
+        """Start measuring time.
 
-    def toc(self, key=''):
+        Create Timer and use as reference for _last_tic.
+        First insert Timer into stack, then measure time to minimize noise.
         """
+        self._last_tic = t = Timer(label=key)
+#        self._timer_order.append(t)  # temporary slot to maintain ordering
+        self._timer_stack.append(t)
+        t.time()
+
+    def toc(self, key=None):
+        """Stop measuring time.
+
         Measure time at the soonest moment possible to not include internal
         processing tasks.
-
-        Todo:
-            * Use label parameter to provide control of what it is paired with
         """
-        t2 = Timer(id=key, **self._kwargs_t)
-        t2.time()
-        tdiff = t2 - self._timer_stack.pop()
-        self.timers.append(tdiff)
+        # No-op, if no tic() has been invoked previously
+        # _last_tic -> _timer
+        if self._last_tic is self._timer:
+            return None
 
-        return tdiff.seconds
+        # Record time
+        self._timer.time()
+
+        # No matching tic(), if stack is empty
+        # _last_tic -> timer from last tic()
+        if self._timer_stack:
+            t_diff = self._timer - self._timer_stack.pop()
+        else:
+            t_diff = self._timer - self._last_tic
+
+        self._timers.append(t_diff)
+        self._timer.clear()  # clear internal Timer
+        return t_diff.seconds
 
     def print_info(self):
-        if self.timers:
-            self.timers[0].print_info()
+        self._timer.print_info()
+
+    def write_to_file(self, fn='', flag='w'):
+        """Save timer contents to a file.
+
+        The *flag* argument controls if the file is overwritten or append.
+        """
+        if not fn:
+            fn = self.name if '.' in self.name else self.name + '.time'
+        with open(fn, flag) as fd:
+            fd.write(self.__str__())
